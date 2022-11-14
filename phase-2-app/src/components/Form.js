08fe2ab2e1b0129c00745805779033fe1ed47669
Jennifer Man <jenniferman702@gmail.com>
2022-11-14 11:04:10 -0500

    Update Form

--- phase-2-app/src/components/Form.js
+++ phase-2-app/src/components/Form.js
@@ -6,7 +6,21 @@ function Form ( {name, country, zipcode, state, city} ) {
 
     return (
         <div id="form" className="weather-form">
-            
+            <form>
+            <label>
+            Name:
+            <input type="text" name="name" />
+            Country:
+            <input type="text" name="country" />
+            Zipcode:
+            <input type="number" name="zipcode" />
+            State:
+            <input type="text" name="state" />
+            City:
+            <input type="text" name="city" />
+            </label>    
+            <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
